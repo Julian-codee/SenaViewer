05ab591c19844e9c5d2f319bcd7874102f2fab63
SENA <mmm808383@gmail.com>
2024-08-15 09:09:11 -0500

    Creacion java class chapter , magazine,movie y serie

--- src/edu/misena/senaviewer/model/book.java
+++ src/edu/misena/senaviewer/model/book.java
@@ -1,4 +1,27 @@
 package edu.misena.senaviewer.model;
+import java.util.Date;
 
 public class book {
+
+    int id;
+    String title;
+    Date edititionDate;
+    String editorial;
+    String authors;
+    int isbn;
+    boolean readed;
+    Date timeReaded;
+    public book(){
+
+    }
+
+    public book (String title , Date edititionDate, String editorial, int isbn)
+    {
+        this.title = title;
+        this.edititionDate = edititionDate;
+        this.editorial = editorial;
+        this.isbn = isbn;
+
+    }
+
 }
