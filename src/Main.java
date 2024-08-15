8c3ec7a1c4dc2fc17857fade7a48b520a7d86294
SENA <mmm808383@gmail.com>
2024-08-15 09:45:40 -0500

    Creacion menu de usuario

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+
+        int opcion;
+
+        do{
+            String message = "\n\n!Bienvenido a SenaViewed¡\n\n";
+
+            message += "1. Book\n";
+            message += "2. Chapter\n";
+            message += "3. Movie\n";
+            message += "4. Serie\n";
+            message += "5. Magazine\n";
+            message += "6. Salir...\n\n";
+
+            System.out.println(message);
+
+            System.out.println("Elige tu Menu: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.println("Ingrese una opción valida");
+                    break;
+
+            }
+
+
+        }while(opcion !=6);
+
+    }
+
+}
