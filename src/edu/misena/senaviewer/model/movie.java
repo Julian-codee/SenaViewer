7fb36dbde68401b7c86b4fc061fb7eafbf4b2d9a
Julian Cortes <mmm808383@gmail.com>
2024-08-16 14:30:26 -0500

    Getter y setter - movie

--- src/edu/misena/senaviewer/model/movie.java
+++ src/edu/misena/senaviewer/model/movie.java
@@ -22,4 +22,89 @@ public class movie {
         this.year = year;
 
     }
+
+    //Getters and Setters
+
+    //id
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    //Title
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    //Genre
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    //Creator
+
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    //Duration
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    //Year
+
+
+    public Date getYear() {
+        return year;
+    }
+
+    public void setYear(Date year) {
+        this.year = year;
+    }
+
+    //Viewed
+
+    public boolean getViewed() {
+        return viewed;
+    }
+
+    public void setViewed(boolean viewed) {
+        this.viewed = viewed;
+    }
+
+    //TimeViewed
+
+
+    public int getTimeViewed() {
+        return timeViewed;
+    }
+
+    public void setTimeViewed(int timeViewed) {
+        this.timeViewed = timeViewed;
+    }
 }
