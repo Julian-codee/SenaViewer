2e4ad47cecb3f9c447ebf95a20c8857c6b9e69f6
Julian Cortes <mmm808383@gmail.com>
2024-08-16 12:43:01 -0500

    Getter y setter - Magazine

--- src/edu/misena/senaviewer/model/magazine.java
+++ src/edu/misena/senaviewer/model/magazine.java
@@ -16,4 +16,57 @@ public class magazine {
         this.editionDate = editionDate;
         this.editorial = editorial;
     }
+
+    //Getters and Setters
+
+    //id
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    //Title
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    //editionDate
+
+    public Date getEditionDate(){
+        return editionDate;
+    }
+
+    public void setEditionDate(Date editionDate) {
+        this.editionDate = editionDate;
+    }
+
+    //Editorial
+
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    //Authors
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
 }
